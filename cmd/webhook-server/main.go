f7b0963cbfe6bba053d55e647231726cc7f54b2d
Robert Tisma <942951+rtisma@users.noreply.github.com>
2020-09-08 19:45:50 -0400

    added proper env variables to deployment and added patch debug

--- cmd/webhook-server/main.go
+++ cmd/webhook-server/main.go
@@ -114,7 +114,14 @@ func applySecurityDefaults(req *v1beta1.AdmissionRequest) ([]patchOperation, err
 		fmt.Println(err)
 		return patches, nil
 	}
-	fmt.Println(string(b))
+	fmt.Println("POD: ",string(b))
+
+
+	//TODO: iterate and dump patches, so they can be applied to incomming request. render and check its ok
+	b2, err2 := json.Marshal(patches)
+	if  err2 != nil {
+		fmt.Println("Patches: ", string(b2))
+	}
 
 	return patches, nil
 }
